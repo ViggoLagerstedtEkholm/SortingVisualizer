9f83ce9c67d7b0ed56faba17012470402a08d80c
ViggoLagerstedtEkholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2020-07-25 15:56:14 +0200

    Added types

---
+++ SortingVisualizer/Algorithms/AlgorithmType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingVisualizer.Algorithms
+{
+    public enum AlgorithmType{
+        BubbleSort = 1,
+        SelectionSort = 2
+    };
+    
+}
