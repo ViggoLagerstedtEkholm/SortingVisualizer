d8df57a16d7c85c14891f375de05f71ddc66d3a4
ViggoLagerstedtEkholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2020-07-26 00:31:47 +0200

    Added quick sort

---
+++ SortingVisualizer/Algorithms/QuickSort.cs
@@ -0,0 +1,102 @@
+﻿using SortingVisualizer.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SortingVisualizer.Algorithms
+{
+    public class QuickSort : ISortAlgorithms
+    {
+        /// <summary>
+        /// QUICK-SORT
+        /// Help with implementation - http://csharpexamples.com/c-quick-sort-algorithm-implementation/
+        /// </summary>
+        private int[] array;
+        private Thread SleepThread = null;
+        public int iterations;
+        private string name = "Quicksort";
+        private int currentlyMoving;
+        private int sleepTime;
+        private bool isDone;
+        private SortingStarter sortingStarter;
+        public QuickSort(int[] array, int sleepTime, SortingStarter sortingStarter)
+        {
+            this.array = array;
+            this.sleepTime = sleepTime;
+            this.sortingStarter = sortingStarter;
+        }
+        public void StartThread()
+        {
+            SleepThread = new Thread(temp);
+            SleepThread.Start();
+        }
+
+        private void temp()
+        {
+            Sort(0, array.Length - 1);
+            Done();
+        }
+        public void Sort(int start, int end)
+        {
+            int i;
+            if (start < end)
+            {
+                i = Partition(start, end);
+
+                Sort(start, i - 1);
+                Sort(i + 1, end);
+            }
+        }
+        private int Partition(int start, int end)
+        {
+            int temp;
+            int p = array[end];
+            int i = start - 1;
+
+            for (int j = start; j <= end - 1; j++)
+            {
+                if (array[j] <= p)
+                {
+                    i++;
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+            temp = array[i + 1];
+            array[i + 1] = array[end];
+            array[end] = temp;
+            currentlyMoving = array[end];
+            Thread.Sleep(sleepTime);
+            iterations++;
+            return i + 1;
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getIterations()
+        {
+            return iterations;
+        }
+
+        public int getCurrentMoving()
+        {
+            return currentlyMoving;
+        }
+
+        public void Done()
+        {
+            sortingStarter.startData();
+            SleepThread.Abort();
+        }
+
+        public void Sort()
+        {}
+    }
+}
