b014401e00979d2b477e5d96cf8fd62bfeddb5a4
ViggoLagerstedtEkholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2020-07-25 15:56:49 +0200

    Added input

---
+++ SortingVisualizer/Setup/Input.cs
@@ -0,0 +1,26 @@
+﻿using SortingVisualizer.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingVisualizer.Setup
+{
+    public class Input
+    {
+        public Input(Window window)
+        {
+            window.KeyDown += Window_KeyDown;
+        }
+
+        public void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
