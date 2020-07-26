cf00aadcadc1aaa4800c31cecbeab134bb728cb8
ViggoLagerstedtEkholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2020-07-26 17:18:56 +0200

    Added comments

--- SortingVisualizer/Setup/Vector2d.cs
+++ SortingVisualizer/Setup/Vector2d.cs
@@ -10,17 +10,28 @@ namespace SortingVisualizer.Setup
     {
         public int X { get; set; }
         public int Y { get; set; }
+        /// <summary>
+        /// Create a Vector2D at (0,0) with the help of setZero()
+        /// </summary>
         public Vector2d()
         {
             setZero();
         }
-
+        
+        /// <summary>
+        /// Creates a Vector2D using the parameter values.
+        /// </summary>
+        /// <param name="X">X INT VALUE</param>
+        /// <param name="Y">Y INT VALUE</param>
         public Vector2d(int X, int Y)
         {
             this.X = X;
             this.Y = Y;
         }
 
+        /// <summary>
+        /// Sets this.X and this.Y to 0;
+        /// </summary>
         private void setZero()
         {
             this.X = 0;
