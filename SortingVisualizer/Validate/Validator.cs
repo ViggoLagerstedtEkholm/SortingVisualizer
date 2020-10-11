c1bdf1a861066c3601beba80594d5b2f0d1155ab
Viggo Lagerstedt Ekholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2020-10-11 03:26:30 +0200

    Added validate class for custom input

---
+++ SortingVisualizer/Validate/Validator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingVisualizer.Validate
+{
+    public static class Validator
+    {
+        public static bool TxfHasContent(TextBox txfBox)
+        {
+            bool hasValue = false;
+
+            if(txfBox.Text != "" || txfBox.TextLength != 0)
+            {
+                hasValue = true;
+            }
+
+            return hasValue;
+        }
+
+        public static bool TxfHasInteger(TextBox txfBox)
+        {
+            bool hasInteger = false;
+            int parsedValue;
+
+            if(int.TryParse(txfBox.Text, out parsedValue))
+            {
+                hasInteger = true;
+            }
+
+            return hasInteger;
+        }
+
+        public static bool CheckBoxListHasValue(CheckedListBox list)
+        {
+            bool hasCheckedItems = false;
+
+            for (int i = 0; i < list.Items.Count; i++)
+            {
+                if (list.GetItemChecked(i))
+                {
+                    hasCheckedItems = true;
+                }
+            }
+
+            return hasCheckedItems;
+        }
+    }
+}
