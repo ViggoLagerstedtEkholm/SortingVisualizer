9a3a9c74170cdfeb03f28b8851bda2c093227647
Viggo <viggo.lagerstedtekholm@gmail.com>
2020-10-13 01:40:42 +0200

    Instead of using Queue we can now use a simple foreach loop combinded  with a thread to show each algorithm 1 by 1.

---
+++ SortingVisualizer/Draw/SortingHandler.cs
@@ -0,0 +1,52 @@
+﻿using SortingVisualizer.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SortingVisualizer.Draw.Windows
+{
+    class SortingHandler 
+    {
+        private List<ISortAlgorithms> algorithms;
+        private Window window;
+        public SortingHandler(List<ISortAlgorithms> algorithms, Window window)
+        {
+            this.algorithms = algorithms;
+            this.window = window;
+            initiateSorting();
+        }
+
+        /// <summary>
+        /// This method goes through the whole array of algorithms and displays the sorting using the window class.
+        /// </summary>
+        private void initiateSorting()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                try
+                {
+                    Thread.Sleep(300);
+                }
+                catch (ThreadInterruptedException exception)
+                {
+                    Console.WriteLine("Thread Interrupted" + exception);
+                }
+
+                foreach (ISortAlgorithms algorithm in algorithms)
+                {
+                    window.setCurrentAlgorithm(algorithm);
+                    window.Shuffle();
+
+                    algorithm.Sort();
+                    window.ResetColor();
+                }
+
+            }).Start();
+        }
+    }
+}
