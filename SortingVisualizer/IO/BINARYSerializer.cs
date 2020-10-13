17309b4f49973ffdba0955dfd7ffeb93c10992b9
Viggo <viggo.lagerstedtekholm@gmail.com>
2020-10-14 00:46:12 +0200

    Added a binary serializer for serializing/deserializing both individual objects and arrays.

---
+++ SortingVisualizer/IO/BINARYSerializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2.IO
+{
+    class BINARYSerializer<T> : ISerializers<T>
+    {
+        private BinaryFormatter formatter;
+        public BINARYSerializer()
+        {
+            formatter = new BinaryFormatter();
+        }
+        public void Serialize<T>(T serializeObject, string filePath, bool append)
+        {
+            using(FileStream outFile = new FileStream("SortedData.bin", FileMode.Create, FileAccess.Write))
+            {
+                formatter.Serialize(outFile, serializeObject);
+            }
+        }
+
+        public T Deserialize(string path)
+        {
+            T objectReturned;
+
+            using (FileStream outFile = new FileStream("SortedData.bin", FileMode.Open, FileAccess.Read))
+            {
+                objectReturned = (T)formatter.Deserialize(outFile);
+            }
+
+            return objectReturned;
+        }
+
+        public T[] DeserializeList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] SerializeList(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
