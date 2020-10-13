bb16d6024af0946707f69fcd22c1ce81d9ac6d74
Viggo <viggo.lagerstedtekholm@gmail.com>
2020-10-14 00:47:44 +0200

    Added a JSON serializer.

---
+++ SortingVisualizer/IO/JSONSerializer.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2.IO
+{
+    class JSONSerializer<T> : ISerializers<T>
+    {
+        private JsonSerializer jsonSerializer;
+        private JsonSerializerSettings settings;
+        public JSONSerializer()
+        {
+            jsonSerializer = new JsonSerializer();
+            settings = new JsonSerializerSettings { Formatting = Newtonsoft.Json.Formatting.Indented };
+        }
+
+        /// <summary>
+        /// Serializes a given object into a XML file.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="serializeObject">Object to serialize</param>
+        /// <param name="fileName">Name of the file we want to save.</param>
+        public void Serialize<T>(T serializeObject, string filePath, bool append)
+        {
+            string jsonData = JsonConvert.SerializeObject(serializeObject, settings);
+            using(StreamWriter writer = new StreamWriter("SortedData.json"))
+            {
+                writer.Write(jsonData);
+            }
+        }
+        public T Deserialize(string path)
+        {
+            T objectReturned;
+
+            using(StreamReader reader = File.OpenText("SortedData.json"))
+            {
+                objectReturned = (T)jsonSerializer.Deserialize(reader, typeof(T));
+            }
+
+            return objectReturned;
+        }
+
+        public T[] DeserializeList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] SerializeList(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
