c7f01ef4fd69368f1025abf1b6a3f6250158834c
Viggo <viggo.lagerstedtekholm@gmail.com>
2020-10-14 00:48:49 +0200

    Added Newtonsoft json serializer

--- SortingVisualizer/Program.cs
+++ SortingVisualizer/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp2.IO;
 
 namespace SortingVisualizer
 {
@@ -15,9 +16,21 @@ namespace SortingVisualizer
         [STAThread]
         static void Main()
         {
+
+            //Book book = new Book();
+            //ISerializers<Book> xmlSerializer = new XMLSerializer<Book>();
+            //xmlSerializer.Serialize(book, "C:\\SerializedObjects", false);
+
+            ISerializers<Book> txtSerializer = new JSONSerializer<Book>();
+            Book book = new Book();
+            
+                txtSerializer.Serialize(book.name, "‪C:\\Users\\Viggo\\Desktop\\TESTING.txt", false);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Start());
+
+
         }
     }
 }
