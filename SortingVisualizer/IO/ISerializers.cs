af93fd0b6e851c42253d204d13cd3c24045a3eba
Viggo <viggo.lagerstedtekholm@gmail.com>
2020-10-14 00:46:31 +0200

    Added a generic interface which is used by all serializers

---
+++ SortingVisualizer/IO/ISerializers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2.IO
+{
+    interface ISerializers<T>
+    {
+        void Serialize<T>(T serializeObject, string filePath, bool append);
+        T Deserialize(string path);
+        T[] DeserializeList();
+        T[] SerializeList(List<T> list);
+
+    }
+}
