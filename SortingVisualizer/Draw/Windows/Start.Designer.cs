d6cd84a90aeb00d2768b6e1d213b09cf71d01c17
Viggo <viggo.lagerstedtekholm@gmail.com>
2020-10-15 00:22:24 +0200

    Added componets

--- SortingVisualizer/Draw/Windows/Start.Designer.cs
+++ SortingVisualizer/Draw/Windows/Start.Designer.cs
@@ -30,10 +30,15 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sleepTimeBar = new System.Windows.Forms.TrackBar();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.ResumeBtn = new System.Windows.Forms.Button();
+            this.PauseBtn = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.AlgorithmsList = new System.Windows.Forms.CheckedListBox();
-            this.btnStart = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.AlgorithmsList = new System.Windows.Forms.CheckedListBox();
+            this.selectedAlgorithmListBox = new System.Windows.Forms.ListBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.MaxTxf = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,9 +46,13 @@
             this.BarCountTxf = new System.Windows.Forms.TextBox();
             this.MinTxf = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.selectedAlgorithmListBox = new System.Windows.Forms.ListBox();
+            this.SortingPanel = new System.Windows.Forms.Panel();
+            this.sineWave = new System.Windows.Forms.RadioButton();
+            this.regularArray = new System.Windows.Forms.RadioButton();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sleepTimeBar)).BeginInit();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -52,69 +61,125 @@
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.sleepTimeBar);
+            this.panel1.Controls.Add(this.StopBtn);
+            this.panel1.Controls.Add(this.ResumeBtn);
+            this.panel1.Controls.Add(this.PauseBtn);
             this.panel1.Controls.Add(this.panel4);
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(1277, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(933, 488);
+            this.panel1.Size = new System.Drawing.Size(386, 937);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(291, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Sleep time";
+            // 
+            // sleepTimeBar
+            // 
+            this.sleepTimeBar.Location = new System.Drawing.Point(255, 147);
+            this.sleepTimeBar.Maximum = 100;
+            this.sleepTimeBar.Minimum = 1;
+            this.sleepTimeBar.Name = "sleepTimeBar";
+            this.sleepTimeBar.Size = new System.Drawing.Size(116, 45);
+            this.sleepTimeBar.TabIndex = 14;
+            this.sleepTimeBar.Value = 1;
+            this.sleepTimeBar.Scroll += new System.EventHandler(this.sleepTimeBar_Scroll);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(255, 78);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(126, 23);
+            this.StopBtn.TabIndex = 13;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // ResumeBtn
+            // 
+            this.ResumeBtn.Location = new System.Drawing.Point(255, 45);
+            this.ResumeBtn.Name = "ResumeBtn";
+            this.ResumeBtn.Size = new System.Drawing.Size(126, 23);
+            this.ResumeBtn.TabIndex = 12;
+            this.ResumeBtn.Text = "Resume";
+            this.ResumeBtn.UseVisualStyleBackColor = true;
+            this.ResumeBtn.Click += new System.EventHandler(this.ResumeBtn_Click);
+            // 
+            // PauseBtn
+            // 
+            this.PauseBtn.Location = new System.Drawing.Point(255, 16);
+            this.PauseBtn.Name = "PauseBtn";
+            this.PauseBtn.Size = new System.Drawing.Size(126, 23);
+            this.PauseBtn.TabIndex = 11;
+            this.PauseBtn.Text = "Pause";
+            this.PauseBtn.UseVisualStyleBackColor = true;
+            this.PauseBtn.Click += new System.EventHandler(this.PauseBtn_Click);
             // 
             // panel4
             // 
-            this.panel4.Controls.Add(this.selectedAlgorithmListBox);
+            this.panel4.Controls.Add(this.panel3);
+            this.panel4.Controls.Add(this.panel2);
             this.panel4.Controls.Add(this.btnStart);
-            this.panel4.Controls.Add(this.AlgorithmsList);
-            this.panel4.Location = new System.Drawing.Point(242, 14);
+            this.panel4.Location = new System.Drawing.Point(14, 3);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(438, 377);
+            this.panel4.Size = new System.Drawing.Size(235, 929);
             this.panel4.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.regularArray);
+            this.panel3.Controls.Add(this.sineWave);
+            this.panel3.Controls.Add(this.AlgorithmsList);
+            this.panel3.Controls.Add(this.selectedAlgorithmListBox);
+            this.panel3.Location = new System.Drawing.Point(14, 184);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(208, 674);
+            this.panel3.TabIndex = 1;
             // 
             // AlgorithmsList
             // 
             this.AlgorithmsList.FormattingEnabled = true;
-            this.AlgorithmsList.Location = new System.Drawing.Point(14, 16);
+            this.AlgorithmsList.Location = new System.Drawing.Point(15, 19);
             this.AlgorithmsList.Name = "AlgorithmsList";
-            this.AlgorithmsList.Size = new System.Drawing.Size(197, 349);
+            this.AlgorithmsList.Size = new System.Drawing.Size(173, 274);
             this.AlgorithmsList.TabIndex = 1;
             this.AlgorithmsList.SelectedIndexChanged += new System.EventHandler(this.AlgorithmsList_SelectedIndexChanged);
             // 
-            // btnStart
+            // selectedAlgorithmListBox
             // 
-            this.btnStart.Location = new System.Drawing.Point(217, 266);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(205, 99);
-            this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.panel2);
-            this.panel3.Location = new System.Drawing.Point(11, 14);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(225, 377);
-            this.panel3.TabIndex = 9;
+            this.selectedAlgorithmListBox.FormattingEnabled = true;
+            this.selectedAlgorithmListBox.Location = new System.Drawing.Point(15, 314);
+            this.selectedAlgorithmListBox.Name = "selectedAlgorithmListBox";
+            this.selectedAlgorithmListBox.Size = new System.Drawing.Size(173, 225);
+            this.selectedAlgorithmListBox.TabIndex = 1;
             // 
             // panel2
             // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.MaxTxf);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.BarAmount);
             this.panel2.Controls.Add(this.BarCountTxf);
             this.panel2.Controls.Add(this.MinTxf);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(12, 16);
+            this.panel2.Location = new System.Drawing.Point(14, 13);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 165);
+            this.panel2.Size = new System.Drawing.Size(208, 165);
             this.panel2.TabIndex = 8;
             // 
             // MaxTxf
             // 
             this.MaxTxf.Location = new System.Drawing.Point(6, 83);
             this.MaxTxf.Name = "MaxTxf";
-            this.MaxTxf.Size = new System.Drawing.Size(177, 20);
+            this.MaxTxf.Size = new System.Drawing.Size(193, 20);
             this.MaxTxf.TabIndex = 7;
             // 
             // label1
@@ -139,14 +204,15 @@
             // 
             this.BarCountTxf.Location = new System.Drawing.Point(6, 130);
             this.BarCountTxf.Name = "BarCountTxf";
-            this.BarCountTxf.Size = new System.Drawing.Size(177, 20);
+            this.BarCountTxf.Size = new System.Drawing.Size(193, 20);
             this.BarCountTxf.TabIndex = 2;
             // 
             // MinTxf
             // 
+            this.MinTxf.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.MinTxf.Location = new System.Drawing.Point(6, 34);
             this.MinTxf.Name = "MinTxf";
-            this.MinTxf.Size = new System.Drawing.Size(177, 20);
+            this.MinTxf.Size = new System.Drawing.Size(193, 20);
             this.MinTxf.TabIndex = 3;
             // 
             // label2
@@ -158,31 +224,69 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Max";
             // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(14, 864);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(202, 62);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // selectedAlgorithmListBox
+            // SortingPanel
             // 
-            this.selectedAlgorithmListBox.FormattingEnabled = true;
-            this.selectedAlgorithmListBox.Location = new System.Drawing.Point(217, 16);
-            this.selectedAlgorithmListBox.Name = "selectedAlgorithmListBox";
-            this.selectedAlgorithmListBox.Size = new System.Drawing.Size(205, 238);
-            this.selectedAlgorithmListBox.TabIndex = 1;
+            this.SortingPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SortingPanel.Location = new System.Drawing.Point(12, 12);
+            this.SortingPanel.Name = "SortingPanel";
+            this.SortingPanel.Size = new System.Drawing.Size(1259, 937);
+            this.SortingPanel.TabIndex = 1;
+            // 
+            // sineWave
+            // 
+            this.sineWave.AutoSize = true;
+            this.sineWave.Location = new System.Drawing.Point(15, 545);
+            this.sineWave.Name = "sineWave";
+            this.sineWave.Size = new System.Drawing.Size(75, 17);
+            this.sineWave.TabIndex = 2;
+            this.sineWave.TabStop = true;
+            this.sineWave.Text = "Sine wave";
+            this.sineWave.UseVisualStyleBackColor = true;
+            this.sineWave.CheckedChanged += new System.EventHandler(this.sineWave_CheckedChanged);
+            // 
+            // regularArray
+            // 
+            this.regularArray.AutoSize = true;
+            this.regularArray.Location = new System.Drawing.Point(103, 545);
+            this.regularArray.Name = "regularArray";
+            this.regularArray.Size = new System.Drawing.Size(62, 17);
+            this.regularArray.TabIndex = 3;
+            this.regularArray.TabStop = true;
+            this.regularArray.Text = "Regular";
+            this.regularArray.UseVisualStyleBackColor = true;
+            this.regularArray.CheckedChanged += new System.EventHandler(this.regularArray_CheckedChanged);
             // 
             // Start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(957, 512);
+            this.ClientSize = new System.Drawing.Size(1675, 961);
+            this.Controls.Add(this.SortingPanel);
             this.Controls.Add(this.panel1);
             this.Name = "Start";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Start_Load);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sleepTimeBar)).EndInit();
             this.panel4.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
@@ -196,7 +300,6 @@
         private System.Windows.Forms.CheckedListBox AlgorithmsList;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TextBox MaxTxf;
         private System.Windows.Forms.Label label1;
@@ -205,6 +308,15 @@
         private System.Windows.Forms.TextBox MinTxf;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListBox selectedAlgorithmListBox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel SortingPanel;
+        private System.Windows.Forms.Button PauseBtn;
+        private System.Windows.Forms.Button ResumeBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar sleepTimeBar;
+        private System.Windows.Forms.RadioButton regularArray;
+        private System.Windows.Forms.RadioButton sineWave;
     }
 }
 
