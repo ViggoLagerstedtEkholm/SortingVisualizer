6c61f86f3fbca23b084da7840e33dec03f8337ae
ViggoLagerstedtEkholm <viggo.lagerstedtekholm@gmail.com>
2020-10-20 01:30:00 +0200

    Added object to serialize.

---
+++ SortingVisualizer/Draw/SortSummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2.Draw
+{
+    public class SortSummary
+    {
+        public string name { get; set; }
+        public int iterations { get; set; }
+        public int sleepTime { get; set; }
+        public int[] sortedArray { get; set; }
+        public int[] unsortedArray { get; set; }
+        public SortSummary()
+        {}
+    }
+}
